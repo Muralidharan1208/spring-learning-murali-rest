6d4bc8f8c8b9189eae46ce73afdc526741d05b29
Muralidharan1208 <136390511+Muralidharan1208@users.noreply.github.com>
2024-01-24 19:28:29 +0530

    my-spring-learning-source-code
    my-spring-learning-source-code

---
+++ src/main/java/com/murali/rest/SpringLearningMuraliRestApplication.java
@@ -0,0 +1,13 @@
+package com.murali.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningMuraliRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningMuraliRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murali/rest/SpringLearningMuraliRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murali.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningMuraliRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
